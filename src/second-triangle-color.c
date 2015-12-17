5401e11229b1f7c849d3928813456e03029637df
MGageMorgan <gagemorgan@outlook.com>
2015-12-17 15:34:00 -0500

    Update second-triangle-color.c

--- src/second-triangle-color.c
+++ src/second-triangle-color.c
@@ -16,14 +16,14 @@ main (int argc, char *argv[])
             cairo_create (surface); //Sets the Cairo context as a pointer, *cr.
 
 	/* Second triangle */
-	cairo_set_source_rgb (cr, 0.60, 0.85, 0.91); //Sets the color of the triangle.
+	cairo_set_source_rgba (cr, 0.60, 0.85, 0.91, 0.8); //Sets the color of the triangle.
 	cairo_line_to (cr, 275, 44); //Draws a line from Point A to Point B.
 	cairo_line_to (cr, 377, 102); //Draws a line from Point B to Point C.
 	cairo_line_to (cr, 100, 118); //Draws a line from Point C back to Point A.	
 	cairo_close_path (cr); //Finishes the connections between Points A and C, closes off the figure.
 
 	cairo_fill_preserve (cr); //Preserve fill so shape doesn't output as an outline.
-	cairo_set_source_rgb (cr, 0.60, 0.85, 0.91); //Sets the color of the triangle (again)
+	cairo_set_source_rgba (cr, 0.60, 0.85, 0.91, 0.8); //Sets the color of the triangle (again)
 	cairo_stroke (cr); //Draw the final shape
 
 	/* Write output and clean up */
